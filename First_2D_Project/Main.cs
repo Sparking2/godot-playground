ed890927a85c925d0d45ea1cdb93d161f3602441
spark <sparking02@yahoo.com.mx>
2024-03-09 19:37:40 -0600

    🚀 Add Main class for 2D project with game logic

---
+++ First_2D_Project/Main.cs
@@ -0,0 +1,69 @@
+using Godot;
+
+namespace First_2D_Project;
+
+public partial class Main : Node
+{
+    [Export]
+    public PackedScene MobScene { get; set; }
+
+    private int _score;
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        NewGame();
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process( double delta ) { }
+
+    public void GameOver()
+    {
+        GetNode<Timer>("MobTimer").Stop();
+        GetNode<Timer>("ScoreTimer").Stop();
+    }
+
+    public void NewGame()
+    {
+        _score = 0;
+
+        var player = GetNode<Player>("Player");
+        var startPosition = GetNode<Marker2D>("StartPosition");
+        player.Start(startPosition.Position);
+
+        GetNode<Timer>("StartTimer").Start();
+    }
+
+    private void OnScoreTimerTimeout()
+    {
+        _score++;
+    }
+
+    private void OnStartTimerTimeout()
+    {
+        GD.Print("Hello!");
+        GetNode<Timer>("MobTimer").Start();
+        GetNode<Timer>("ScoreTimer").Start();
+    }
+
+    private void OnMobTimerTimeout()
+    {
+        Mob mob = MobScene.Instantiate<Mob>();
+
+        var mobSpawnLocation = GetNode<PathFollow2D>("Path2D/MobSpawnLocation");
+        mobSpawnLocation.ProgressRatio = GD.Randf();
+
+        float direction = mobSpawnLocation.Rotation + Mathf.Pi / 2;
+
+        mob.Position = mobSpawnLocation.Position;
+
+        direction += (float) GD.RandRange(-Mathf.Pi / 4, Mathf.Pi / 4);
+        mob.Rotation = direction;
+
+        var velocity = new Vector2((float) GD.RandRange(150.0, 250.0), 0);
+        mob.LinearVelocity = velocity.Rotated(direction);
+
+        AddChild(mob);
+    }
+}
